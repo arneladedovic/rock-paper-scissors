667bfccea848e4e093ad7f0df0f4896a7484df27
arneladedovic <arneladedovic@hotmail.com>
2022-07-10 00:16:16 +0000

    Add event listener for each option and function for start play, moves left and random out options

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,11 @@
+// Base
 const game = () => {
   let playerScore = 0;
   let computerScore = 0;
   let moves = 0;
 }
 
+// Function to play game 
 const playGame = () => {
   const rockButton = document.querySelector('.rock');
   const paperButton = document.querySelector('.paper');
@@ -16,7 +18,12 @@ const playGame = () => {
   playerOptions.forEach(option => {
     option.addEventListener('click', function() {
 
+    const movesLeft = document.querySelector('.movesLeft'); moves++;
+    movesLeft.innerText = `Moves Left: ${10-moves}`;
     })
+
+      const choiceNumber = Math.floor(Math.random()*3);
+      const computerChoice = computerOptions[choiceNumber];
   });
 
   // Function to see who wins
