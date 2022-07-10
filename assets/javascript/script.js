d7997f08b387795ec4f33f69296859d5d0e9cad3
arneladedovic <arneladedovic@hotmail.com>
2022-07-10 00:04:33 +0000

    Set up structure of game functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,30 @@
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+  let moves = 0;
+}
+
+const playGame = () => {
+  const rockButton = document.querySelector('.rock');
+  const paperButton = document.querySelector('.paper');
+  const scissorButton = document.querySelector('.scissor');
+  const playerOptions = [rockButton, paperButton, scissorButton];
+  const computerOptions = ['rock', 'paper', 'scissor'];
+}
+
+// Function to start play
+  playerOptions.forEach(option => {
+    option.addEventListener('click', function() {
+
+    })
+  });
+
+  // Function to see who wins
+
+  // Function to end game efter 10moves
+
+  // Function to decide winner
+
+  // Function to run when game over
+
+
